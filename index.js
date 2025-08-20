64e7bd2f2b713b1e2dc7ef261cbacb042c999585
Sam <69408828+syntheticsam@users.noreply.github.com>
2025-08-20 17:20:04 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 alert('XSS achieved!!! :3 ~ wntiv_, 162.159.137.232, amsaynz');
-window.addEventListener("load",function(){
-  var audio=document.createElement("audio")
-  audio.src="https://albedo.amsay.xyz/shared/music/embrace-364091.mp3"
-  audio.autoplay=true
-  audio.loop=true
-  document.body.appendChild(audio)
-})
+a = new Audio(); a.src = 'https://albedo.amsay.xyz/shared/music/embrace-364091.mp3'; a.autoplay = true; document.body.appendChild(a);
 // document.querySelector('.userprofile .description div').remove();
