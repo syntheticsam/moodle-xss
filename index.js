b40baaf484deb806de675eecd75a39cb76b470db
Sam <69408828+syntheticsam@users.noreply.github.com>
2025-08-20 17:18:18 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 alert('XSS achieved!!! :3 ~ wntiv_, 162.159.137.232, amsaynz');
+window.addEventListener("load",function(){
+  var audio=document.createElement("audio")
+  audio.src="https://albedo.amsay.xyz/shared/music/embrace-364091.mp3"
+  audio.autoplay=true
+  audio.loop=true
+  document.body.appendChild(audio)
+})
 // document.querySelector('.userprofile .description div').remove();
