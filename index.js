4d41423316b010bc312858638665ffe80b5589f6
Sam <69408828+syntheticsam@users.noreply.github.com>
2025-08-20 17:28:02 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 alert('XSS achieved!!! :3 ~ wntiv_, 162.159.137.232, amsaynz');
-a = new Audio(); a.src = 'https://albedo.amsay.xyz/shared/music/embrace-364091.mp3'; a.autoplay = true; document.body.appendChild(a);
 // document.querySelector('.userprofile .description div').remove();
